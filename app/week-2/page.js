c39f3e75b7c648878b1d224380cfba9317fc9dc1
RikudoKen <141680797+RikudoKen@users.noreply.github.com>
2024-05-13 15:15:46 -0600

    Week-2 Update
    Completed

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,9 +1,15 @@
+import Link from "next/link";
+import StudentInfo from "./student-info";
 
 
 export default function Page() {
     return(
         <main>
-            <h1>Shopping List</h1>
+            <h1>Name and Repository</h1>
+            <p>
+            <StudentInfo />
+            </p>
         </main>
+
     );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+
+export default function StudentInfo() {
+
+
+    return(
+        <div>
+            <h2> Kevin Ly</h2>
+            <Link href="https://github.com/RikudoKen/cprg306-assignments">GitHub Repository</Link>
+        </div>
+    )
+}
